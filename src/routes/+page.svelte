f4f0b1cbde83f3c15962d8c865da80d035d01252
anibalmurcia <osirisanibalmurcia@gmail.com>
2024-01-03 11:20:12 -0600

    primer subida

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,15 +1,10 @@
 <script>
     import { Alert } from "flowbite-svelte";
+    import Sidebar from "../routes/sidebar.svelte";
+    import Products from "../routes/products.svelte";
 </script>
 
-<div class="p-8">
-    <Alert border color="blue">
-        <span class="font-medium">Info alert!</span>
-        Change a few things up and try submitting again.
-    </Alert>
+<div class="flex" flex-1 p-4 mt-4>
+    <Sidebar />
+    <Products />
 </div>
-
-<h1>Welcome to SvelteKit</h1>
-<p>
-    Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation
-</p>
---
+++ src/routes/products.svelte
@@ -0,0 +1,93 @@
+<script>
+    import {
+        Table,
+        TableBody,
+        TableBodyCell,
+        TableBodyRow,
+        TableHead,
+        TableHeadCell,
+        Checkbox,
+    } from "flowbite-svelte";
+</script>
+
+<Table>
+    <TableHead>
+        <TableHeadCell class="!p-4">
+            <Checkbox />
+        </TableHeadCell>
+        <TableHeadCell>Producto</TableHeadCell>
+        <TableHeadCell>Tipo</TableHeadCell>
+        <TableHeadCell>Disponible</TableHeadCell>
+        <TableHeadCell>Precio</TableHeadCell>
+        <TableHeadCell>Peso</TableHeadCell>
+        <TableHeadCell>acciones</TableHeadCell>
+    </TableHead>
+    <TableBody class="divide-y">
+        <TableBodyRow>
+            <TableBodyCell class="!p-4">
+                <Checkbox />
+            </TableBodyCell>
+            <TableBodyCell>Tres Leches Personal</TableBodyCell>
+            <TableBodyCell>Tres leches</TableBodyCell>
+            <TableBodyCell>Si</TableBodyCell>
+            <TableBodyCell>L 33</TableBodyCell>
+            <TableBodyCell>240 gramos</TableBodyCell>
+            <TableBodyCell>
+                <a
+                    href="/tables"
+                    class="font-medium text-primary-600 hover:underline dark:text-primary-500"
+                    >Editar</a
+                >
+                <a
+                    href="/tables"
+                    class="font-medium text-red-600 hover:underline dark:text-red-500"
+                    >Remover</a
+                >
+            </TableBodyCell>
+        </TableBodyRow>
+        <TableBodyRow>
+            <TableBodyCell class="!p-4">
+                <Checkbox />
+            </TableBodyCell>
+            <TableBodyCell>Tres Leches Redondo</TableBodyCell>
+            <TableBodyCell>Tres leches</TableBodyCell>
+            <TableBodyCell>Si</TableBodyCell>
+            <TableBodyCell>L 44</TableBodyCell>
+            <TableBodyCell>240 gramos</TableBodyCell>
+            <TableBodyCell>
+                <a
+                    href="/tables"
+                    class="font-medium text-primary-600 hover:underline dark:text-primary-500"
+                    >Editar</a
+                >
+                <a
+                    href="/tables"
+                    class="font-medium text-red-600 hover:underline dark:text-red-500"
+                    >Remover</a
+                >
+            </TableBodyCell>
+        </TableBodyRow>
+        <TableBodyRow>
+            <TableBodyCell class="!p-4">
+                <Checkbox />
+            </TableBodyCell>
+            <TableBodyCell>Tres Leches 15 porciones</TableBodyCell>
+            <TableBodyCell>Tres leches</TableBodyCell>
+            <TableBodyCell>Si</TableBodyCell>
+            <TableBodyCell>L 100</TableBodyCell>
+            <TableBodyCell>240 gramos</TableBodyCell>
+            <TableBodyCell>
+                <a
+                    href="/tables"
+                    class="font-medium text-primary-600 hover:underline dark:text-primary-500"
+                    >Editar</a
+                >
+                <a
+                    href="/tables"
+                    class="font-medium text-red-600 hover:underline dark:text-red-500"
+                    >Remover</a
+                >
+            </TableBodyCell>
+        </TableBodyRow>
+    </TableBody>
+</Table>
